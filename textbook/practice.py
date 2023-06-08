a66d1daddb2bb38cb51f432a978870c61de7034c
Jet1a <jettaphoom@gmail.com>
2023-06-08 17:17:52 +0700

    pyTextbook

--- textbook/practice.py
+++ textbook/practice.py
@@ -120,3 +120,18 @@ def order(n):
 
 n = int(input())      
 order(n)
+
+### Number Array with != n
+
+x = int(input())
+y = int(input())
+z = int(input())
+n = int(input())
+
+array = []
+for i in range(x+1):
+    for j in range(y+1):
+        for k in range(z+1):
+            if i+j+k != n:
+                array.append([i,j,k])
+print(array)
